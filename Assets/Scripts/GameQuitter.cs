5364da44eb6e9df052a5b3f7cd6485dd00df7d25
Osama Ennasr <2357864+ennasros@users.noreply.github.com>
2021-03-06 19:31:41 -0500

    Added escape to quit

---
+++ Assets/Scripts/GameQuitter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuitter : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) { Application.Quit(); }
+    }
+}
